fc5662f03346cafe0e3fccf5023fd72ed8599902
Petr Penzin <penzin.dev@gmail.com>
2018-11-10 08:46:26 -0800

    Start working on Parse definition

--- src/Data/Text/Parse/UTF8.hs
+++ src/Data/Text/Parse/UTF8.hs
@@ -2,6 +2,8 @@
 -- Parse UTF8 strings
 module Data.Text.Parse.UTF8 where
 
+import Data.ByteString.UTF8 as B
+
 import Control.Monad
 #if MIN_VERSION_base(4,9,0)
 import qualified Control.Monad.Fail as Fail
@@ -15,7 +17,11 @@ data ParseStatus = Done | NeedMore | Error
    Inspired by the Get monad, hope is that similar operations can be used on
    UTF8 strings instead of binary data -}
 
-data Parse a = P a -- TODO
+-- | Datatype produced by running Parse
+data Parser a = Fail !B.ByteString
+                -- ^ Parsing failed -- error (can be unexpected end of input)
+              | Partial (Maybe B.ByteString -> Parser a)
+                -- ^ Reached end of input, but can continue
 
 {- Need instances for monad composition, plus representation for partial
    parsing -}
